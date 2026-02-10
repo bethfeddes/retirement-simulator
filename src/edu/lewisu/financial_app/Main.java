ea27e9d42f04d4ed51f6700dc98babad06c36198
Bethany Feddes <befeddes@gmail.com>
2026-02-09 23:36:07 -0600

    Added input validation helpers
    Finished input validation for reading ints, doubles, and strings within
    range.

--- src/edu/lewisu/financial_app/Main.java
+++ src/edu/lewisu/financial_app/Main.java
@@ -15,49 +15,88 @@ public class Main {
 	}
 
 	// Helper to get user input
-	private static String getUserInput ()
+	private static String getUserInput(Scanner sc, String prompt) {
+		System.out.println(prompt);
+		return sc.nextLine();
+	}
 	
-	// Helper method to read int
+	// Helper method to read int in range
 	private static int readIntInRange(Scanner sc, String prompt, int min, int max) {
 		
 		Integer value = null;
 		
+		// Steps to validate input/prevent crash
 		do {
 			
-			System.out.println(prompt);
-			value = sc.nextInt();
+			String userInput = getUserInput(sc, prompt);
 			
-			// 
-			if (value >= min && value <= max) { 
-				
-				
-				
+			try {
+				value = Integer.parseInt(userInput);
+				if(value < min || value > max) {
+					throw new OutOfRangeException(
+						"Error: Enter a value between " + min + " and " + max);
+				}
 			}
-			
-			else {
-				
-				System.out.println("Please enter an integer within range.");
-				
+			catch(NumberFormatException e ) {
+				System.out.println("Invalid Input: Please enter an integer");
 			}
-		
+			catch(OutOfRangeException e) { // Custom exception
+				value = null;
+				System.out.println(e.getMessage());
+			}
+			catch(Exception e) {
+				System.out.println("An error occurred");
+			}
 		} while(value == null);
 	
-		return intInput;
+		return value; // Return validated value
 	}
-	
+	 
 	// Helper method to read double
-	private static void readDoubleInRange(Scanner sc, String prompt, double min, double max) {
+	private static double readDoubleInRange(Scanner sc, String prompt, double min, double max) {
 		
+		Double value = null;
+				
+				// Steps to validate input/prevent crash
+				do {
+					
+					String userInput = getUserInput(sc, prompt);
+					
+					try {
+						value = Double.parseDouble(userInput);
+						if(value < min || value > max) {
+							throw new OutOfRangeException(
+								"Error: Enter a value between " + min + " and " + max);
+						}
+					}
+					catch(NumberFormatException e ) {
+						System.out.println("Invalid Input: Please enter a number");
+					}
+					catch(OutOfRangeException e) {
+						value = null;
+						System.out.println(e.getMessage());
+					}
+					catch(Exception e) {
+						System.out.println("An error occurred");
+					}
+				} while(value == null);
+			
+				return value;
+			}
 		
-		
+	// Helper method to read string input MIGHT NOT NEED
+	private static String readString(Scanner sc, String prompt) {
+		return getUserInput(sc, prompt);
 	}
 	
 	// Helper method to read compounding choice
 	private static int readCompoundingChoice(Scanner sc) {
 		
+		int choice = readIntInRange(sc, "Choose your compounding frequency (1-3):", 1, 3);
 		
-		return 0; // Change later
+		return choice;
 	}
+	
 	
 	// Helper method to run simulation
 	private static void runSimulation(Scanner sc) {
@@ -73,4 +112,19 @@ public class Main {
 		// return true or false
 	}
 	
+	// Custom exception
+	static class OutOfRangeException extends Exception {
+		private static final long serialVersionUID = 1L;
+		public OutOfRangeException() {
+			super();
+		}
+		public OutOfRangeException(String message) {
+			super(message);
+		}
+		public OutOfRangeException(Throwable cause) {
+			super(cause);
+		}
+		public OutOfRangeException(String message, Throwable cause) {
+			super(message, cause);
+		}
 }
